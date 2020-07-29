ae772992864d93ab94723616aaa57de697e5d672
CharlesYWL <xlyin@ucdavis.edu>
2020-07-29 16:00:50 -0700

    disable whitenoise

--- helloword_project/settings.py
+++ helloword_project/settings.py
@@ -169,16 +169,16 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
