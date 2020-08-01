7de96e2044a83cabd4569ed8230d75a3cf65ac18
CharlesYWL <xlyin@ucdavis.edu>
2020-07-31 18:18:18 -0700

    enable media

--- helloword_project/settings.py
+++ helloword_project/settings.py
@@ -169,9 +169,9 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
