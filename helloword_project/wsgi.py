8540dc47abc97a27f2657593c659734891ca320e
CharlesYWL <xlyin@ucdavis.edu>
2020-08-01 08:31:15 +0000

    halfway on apache

--- helloword_project/wsgi.py
+++ helloword_project/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/DjangoExp')
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('PYTHON_EGG_CACHE',"/opt/bitnami/apps/django/django_projects/DjangoExp/egg_cache")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloword_project.settings')
 
 application = get_wsgi_application()
